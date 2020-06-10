ebd201ddca470a1028b7d8bee4dda4fc431df8c6
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-06-10 15:18:56 +0200

    Replace echo commands with config template

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-echo "hostname "$Hostname"" >> /home/cs/csgo_server/csgo/cfg/server.cfg
-echo "sv_maxrate 0" >> /home/cs/csgo_server/csgo/cfg/server.cfg
-echo "sv_minrate 80000" >> /home/cs/csgo_server/csgo/cfg/server.cfg
-echo "sm_cvar sv_maxcmdrate 128" >> /home/cs/csgo_server/csgo/cfg/server.cfg
-echo "sv_mincmdrate 128" >> /home/cs/csgo_server/csgo/cfg/server.cfg
-echo "sv_minupdaterate 128" >> /home/cs/csgo_server/csgo/cfg/server.cfg
-echo "sm_cvar sv_maxupdaterate 128" >> /home/cs/csgo_server/csgo/cfg/server.cfg
 
+# Template out the server config
+sh -c 'echo "'"$(cat server.cfg.template)"'" >> /home/cs/csgo_server/csgo/cfg/server.cfg'
+
+# Set groups and permissions
 groupadd cs -g ${GID:-1000}
 useradd -u ${UID:-1000} -g ${GID:-1000} cs
-
 chown -R cs:cs /home/cs/csgo_server
-su cs -c '/home/cs/csgo_server/srcds_run -game csgo -console -usercon -maxplayers_override $MAXPLAYERS +game_type $GAMETYPE  +game_mode $GAMEMODE +mapgroup $MAPGROUP +map $STARTMAP +sv_setsteamaccount $TOKEN -authkey $WORKSHOPTOKEN +rcon_password $RCONPW +sv_password $PW +sv_region $REGION -tickrate $TICKRATE -net_port_try 1'
+
+# Run the server as 'cs'
+su cs -c '/home/cs/csgo_server/srcds_run\
+	-authkey $WORKSHOPTOKEN\
+	-console -usercon\
+	-game csgo\
+	-maxplayers_override $MAXPLAYERS\
+	-net_port_try 1\
+	-tickrate $TICKRATE'
